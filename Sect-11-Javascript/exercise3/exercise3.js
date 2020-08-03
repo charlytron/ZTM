dfe0bff6ba39686b80eb46da078881957123f9fc
Charles Tompkins <ctompkins32@gmail.com>
2020-08-03 12:43:29 -0500

    Add files via upload

---
+++ Sect-11-Javascript/exercise3/exercise3.js
@@ -0,0 +1,12 @@
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.
+
+// BONUS: Make a program that can subtract, multiply, and also divide!
+
+var firstNum = prompt("gimme the first number");
+var secondNum = prompt("gimme the second number");
+var summa = Number(firstNum) + Number(secondNum);
+alert("your answer is " + summa);
